f731a534aaed4beafb4a9193cefbbeaf7ba59a3d
yash1314 <yashkeshari79@gmail.com>
2024-01-26 10:49:11 +0530

    app.py major changes

--- app.py
+++ app.py
@@ -10,30 +10,42 @@ def model_load(path):
 model = model_load('model/model1.joblib')
 
 
-st.title('Diamond Price Prediction')
+st.title(':rainbow[Diamond Price Prediction]:rainbow')
+st.markdown('----------------------------------------------')
 st.markdown('The goal is to predict price of given diamond using various parameters. The model has an accuracy score of 97.3%')
 st.markdown(' ')
 cols1, cols2 = st.columns(2)
 
 with cols1:
-    st.markdown('##### Carat')
-    carat = st.text_input('Range : 0.2-3.5: ')
-    st.markdown('##### Cut')
-    cut = st.text_input('Fair: 1, Good: 2, Very Good: 3, Premium: 4, Ideal: 5: ')
-    st.markdown('##### Color')
-    color = st.text_input('Color range : D:1, E:2 ,F:3 ,G:4 , H:5 , I:6, J:7')
+    st.markdown('#### Carat')
+    carat = st.slider(" ", 0.2, 3.5)
+
+    st.markdown('#### Clarity')
+    clarity = st.slider(" ", 1, 10)
+
+    st.markdown('#### Depth')
+    depth = st.slider(" ", 52, 72)
+
 
 with cols2:
-    st.markdown('##### Clarity')
-    clarity = st.text_input('Range: 1-10, in increasing order of clarity.')
-    st.markdown('##### Depth')
-    depth = st.text_input('Depth range: 52-72')
-    st.markdown('##### Table')
-    table = st.text_input('Table range: 49-79')
+    st.markdown('#### Table')
+    table = st.slider(" ", 49, 79)
+
+
+    st.markdown('#### Cut')
+    cut = st.selectbox("Cut range : Fair, Good, Very Good, Premium, Ideal",
+                       ["Fair", "Good", "Very Good", "Premium", "Ideal"])
+
+
+    st.markdown('#### Color')
+    color = st.selectbox('Color range : D, E, F, G, H, I, J',
+                         ['D', 'E', 'F', 'G', 'H', 'I', 'J'])
+
 
 if st.button('Predict'):
+    color = {'D':1, 'E':2, 'F':3, 'G':4, 'H':5, 'I':6, 'J':7}.get(color)
+    cut = {'Fair': 1, 'Good': 2, 'Very Good': 3, 'Premium': 4, 'Ideal': 5}.get(cut)
 
-    
     try:
         # 2. predicts
         result = model.predict([[carat, cut, color, clarity, depth, table]])
@@ -47,8 +59,4 @@ if st.button('Predict'):
         bar.progress(100)
         
         # 3. result display
-        st.header(f'The predicted price of diamond is: {result}$')
-
-
-
-
+        st.subheader('The predicted price of diamond is: {} $'.format(result))
